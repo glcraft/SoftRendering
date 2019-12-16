298a65b6f008d5da85067bf12665c9cfa614dceb
Gabin <gabin.lefranc@gmail.com>
2019-12-16 14:59:52 +0100

    abs -> glm::abs Meilleure intégration type dynamique (nombre flottant)

--- CustomFrame.cpp
+++ CustomFrame.cpp
@@ -66,7 +66,7 @@ void CustomFrame::draw_line(glm::tvec3<uint8_t>* pixs, const Command& cmd)
 {
     // https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
     glm::vec2 delta = cmd.pos2-cmd.pos1;
-    float deltaErr = abs(delta.y/delta.x);
+    float deltaErr = glm::abs(delta.y/delta.x);
     float error=0;
     int y=0;
     for (int x=cmd.pos1.x; x<cmd.pos2.x;++x)
