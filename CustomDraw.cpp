492661f8d9d37c17f1ddcc66aacb5c8ea818a381
Gabin <gabin.lefranc@gmail.com>
2019-12-16 19:38:06 +0100

    Correction fonctionnement générateur ligne

--- CustomDraw.cpp
+++ CustomDraw.cpp
@@ -2,16 +2,21 @@
 #include "constants.h"
 #include <algorithm>
 #include <numeric>
-std::vector<std::pair<int, int>> genBresenhamLine(glm::ivec2* pos)
+std::vector<std::pair<int, int>> genBresenhamLine(glm::ivec2* pos_orig)
 {
     // https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+    glm::ivec2 pos[2] = {pos_orig[0], pos_orig[1]};
+    bool reverse=false;
     if (pos[0].x>pos[1].x)
+    {
         std::swap(pos[0], pos[1]);
+        reverse=true;
+    }
     glm::vec2 delta = pos[1]-pos[0];
     if (delta.y==0)
         return {{pos[0].x, pos[1].x}};
     
-    std::vector<std::pair<int, int>> result(glm::abs(pos[1].x-pos[0].x));
+    std::vector<std::pair<int, int>> result(glm::abs(pos[1].y-pos[0].y)+1);
     if (delta.x==0)
     {
         int i=0;
@@ -27,12 +32,15 @@ std::vector<std::pair<int, int>> genBresenhamLine(glm::ivec2* pos)
         error+=deltaErr;
         if (error>= 0.5f)
             while (error>= 0.5f)
-        {
+            {
                 result[i].second=x;
-            error-=1.f;
+                error-=1.f;
                 result[++i].first=x+(error< 0.5f);
             }
     }
+    result.back().second=pos[1].x;
+    if (reverse)
+        return std::vector<std::pair<int, int>>(result.rbegin(), result.rend());
     return result;
 }
 void draw_horizontal(glm::tvec3<uint8_t>* pixs, int y, std::pair<int, int> xs, glm::tvec3<uint8_t> color)
@@ -43,7 +51,6 @@ void draw_horizontal(glm::tvec3<uint8_t>* pixs, int y, std::pair<int, int> xs, g
 }
 void CustomFrame::draw_line(glm::tvec3<uint8_t>* pixs, const Command& cmd)
 {
-    
     glm::ivec2 pos[2]={cmd.pos[0], cmd.pos[1]};
     auto bresLine = genBresenhamLine(pos);
     int signDeltaY=glm::sign(pos[1].y-pos[0].y);
@@ -54,8 +61,33 @@ void CustomFrame::draw_line(glm::tvec3<uint8_t>* pixs, const Command& cmd)
         y+=signDeltaY;
     }
 
-        }
+}
+void CustomFrame::draw_triangle(glm::tvec3<uint8_t>* pixs, const Command& cmd)
+{
+    // http://www.sunshine2k.de/coding/java/TriangleRasterization/TriangleRasterization.html
+    const auto flatTop = [pixs](glm::ivec2 pos[3]){
+        glm::ivec2 posx[2]={pos[1], pos[2]};
+        if (pos[1].x<=pos[2].x)
+            posx[0]=pos[1], posx[1]=pos[2];
+        else
+            posx[0]=pos[2], posx[1]=pos[1];
+        
+        glm::vec2 delta[2] = {posx[0]-pos[0], posx[1]-pos[0]};
+    };
+    const auto flatBottom = [pixs](glm::ivec2 pos[3]){
+
+    };
+    glm::ivec2 pos[3]={cmd.pos[0], cmd.pos[1], cmd.pos[2]};
+    std::sort(std::begin(pos), std::end(pos), [](const glm::ivec2& p1, const glm::ivec2& p2) { return p1.y<p2.y; });
+    if (pos[1].y==pos[2].y)
+        flatTop(pos);
+    else if (pos[1].y==pos[0].y)
+        flatBottom(pos);
+    else
+    {
+        /* code */
     }
+    
 }
 void CustomFrame::clear_image(glm::tvec3<uint8_t>* pixs, const Command& cmd)
 {
