c93f907a7ea1d5c07eaaac4de478f2893d2219cf
GLCraft <gabin.lefranc@gmail.com>
2019-12-22 18:41:50 +0100

    Test : ratio 4:3

--- constants.h
+++ constants.h
@@ -1,7 +1,7 @@
 #pragma once
 namespace Constants
 {
-    constexpr int WindowWidth = 1920, WindowHeight = 1080;
+    constexpr int WindowWidth = 1440, WindowHeight = 1080;
     constexpr int PixelSize = 8;
     constexpr int TextureWidth = WindowWidth/PixelSize, TextureHeight = WindowHeight/PixelSize;
 }
