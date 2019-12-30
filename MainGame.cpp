59ba06800719d10bcefcae2d0918fb78c7d1b69f
Gabin Lefranc <gabin.lefranc@gmail.com>
2019-12-30 18:34:56 +0100

    Adding example stuff

--- MainGame.cpp
+++ MainGame.cpp
@@ -30,44 +30,20 @@ void MainGame::init()
 
     m_cframe = std::unique_ptr<CustomFrame>(new CustomFrame);
     m_cmdBuffer.clear_image(glm::tvec3<uint8_t>(0,0,0));
-    m_cmdBuffer.draw_triangle({1,-1}, {0, 0.8f}, {-1,1}, {0,0.5f,0});
-    m_cmdBuffer.draw_triangle({-1,-1}, {0, -0.8f}, {1,1}, {0.5f,0,0});
 
-    glm::mat4 viewmat = glm::ortho(-16.f/9.f, 16.f/9.f, -1.f, 1.f, -10.f, 10.f);
     const float size=0.5f;
 
     m_texture = std::make_unique<CustomTexture>();
-    m_texture->load("untitled.png");
+    m_texture->load("res/test.png");
     m_vShader=std::make_unique<VertexShader>();
-    m_vShader->m_viewmat = viewmat;
+    m_vShader->m_projmat = glm::perspective(glm::radians(120.f), static_cast<float>(Constants::WindowWidth)/static_cast<float>(Constants::WindowHeight), 1.f, 20.f);
+    m_vShader->m_viewmat = glm::lookAt(glm::vec3(0.f), glm::vec3(1.f,0,0), glm::vec3(0,1,0));
     auto fShader = std::make_unique<FragmentTextureShader>();
     fShader->m_texture.reset(m_texture.get());
     m_fShader = std::move(fShader);
     
     m_vboTri.reset(new VertexBuffer);
-    load_model_obj("untitled.obj",*m_vboTri);
-
-    // m_vboTri->verts.resize(9);
-    
-    // for (size_t i1=0;i1<3;i1++)
-    // {
-    //     size_t reali1=i1*3;
-    //     float angle1 = static_cast<float>(i1)/3.f*2*glm::pi<float>() + 1.f;
-    //     glm::vec2 pos1(glm::cos(angle1)*size, glm::sin(angle1)*size);
-
-    //     for (size_t i2=0;i2<3;i2++)
-    //     {
-    //         float angle = static_cast<float>(i2-i1+3)/3.f*2*glm::pi<float>() + 1.f;
-    //         glm::vec2 pos(glm::cos(angle)*size, glm::sin(angle)*size);
-    //         m_vboTri->verts[reali1+i2].pos=glm::vec4(pos1+pos, 0.f, 1.f);
-    //     }
-    //     m_vboTri->verts[reali1+0].color=glm::vec3(1,0,0);
-    //     m_vboTri->verts[reali1+1].color=glm::vec3(0,1,0);
-    //     m_vboTri->verts[reali1+2].color=glm::vec3(0,0,1);
-    //     m_vboTri->verts[reali1+0].uv=glm::vec2(0,0);
-    //     m_vboTri->verts[reali1+1].uv=glm::vec2(1,0);
-    //     m_vboTri->verts[reali1+2].uv=glm::vec2(0,1);
-    // }
+    load_model_obj("res/trees.obj",*m_vboTri);
 
     {
         auto cmd = m_cmdBuffer.draw_buffer(util::makeObserver(m_vboTri.get()));
@@ -118,7 +94,6 @@ void MainGame::render()
         m_vShader->m_modelmat=glm::mat4(1.f);
         // cmdTri->vertShader->m_modelmat = glm::rotate(cmdTri->vertShader->m_modelmat, angle*0.2f, {0,1,0});
         m_vShader->m_modelmat = glm::rotate(m_vShader->m_modelmat, angle*0.4f, {0,1,0});
-        m_vShader->m_modelmat = glm::rotate(m_vShader->m_modelmat, angle, {0,0,1});
     }
     glClearColor(1,0,0,1);
     glClear(GL_COLOR_BUFFER_BIT);
